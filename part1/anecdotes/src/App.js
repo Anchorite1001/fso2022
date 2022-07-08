d27fd5cf684f15216b83e53cb69317c457565779
Anchorite1001 <yangwu.1415@gmail.com>
2022-07-08 22:02:01 +1000

    part1 exercise 1.13

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -12,15 +12,24 @@ const App = () => {
   ]
    
   const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(new Array(anecdotes.length-1).fill(0))
 
   const handleRandomSelect = () => {
     setSelected(Math.floor(Math.random() * anecdotes.length))
+  }
+
+  const handleVote = () => {
+    let copy = [...votes];
+    copy[selected] += 1;
+    setVotes(copy);
   }
   
   return (
     <div>
       {anecdotes[selected]}
+      <p>has {votes[selected]} votes</p>
       <br />
+      <button onClick={handleVote}>vote</button>
       <button onClick={handleRandomSelect}>next anecdote</button>
     </div>
   )
