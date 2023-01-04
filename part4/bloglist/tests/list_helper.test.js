2839ccf0cc29c332b37b8dfe99ff06cd1041aab2
personal <yangwu.1415@gmail.com>
2023-01-05 01:21:04 +1100

    exercise 4.4-4.5

--- part4/bloglist/tests/list_helper.test.js
+++ part4/bloglist/tests/list_helper.test.js
@@ -1,8 +1,48 @@
+const testingLists = require('./BlogList')
 const listHelper = require('../utils/list_helper')
 
-test('dummy returns one', () => {
-    const blogs = []
+describe('dummy', () => {
+    test('dummy returns one', () => {
+        const result = listHelper.dummy(testingLists.blogs)
+        expect(result).toBe(1)
+    })
+})
+
+describe('totalLikes', () => {
     
-    const result = listHelper.dummy(blogs)
-    expect(result).toBe(1)
+    test('when list has only one blog, equals the likes of that', () => {
+        const result = listHelper.totalLikes(testingLists.listWithOneBlog)
+        expect(result).toBe(5)
+    })
+
+    test('when list has multiple blogs, equals the likes of all blogs', () => {
+        const result = listHelper.totalLikes(testingLists.blogs)
+        expect(result).toBe(36)
+    })
+})
+
+describe('favoriteBlog', () => {
+    test('when list has only one blog, equals it', () => {
+        const result = listHelper.favoriteBlog(testingLists.listWithOneBlog)
+        expect(result).toEqual({
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+            __v: 0
+        })
+    })
+
+    test('when list has multiple blogs, equals the one with the most likes', () => {
+        const result = listHelper.favoriteBlog(testingLists.blogs)
+        expect(result).toEqual({
+            _id: '5a422b3a1b54a676234d17f9',
+            title: 'Canonical string reduction',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+            likes: 12,
+            __v: 0
+        })
+    })
 })
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,8 +1,28 @@
-// eslint-disable-next-line no-unused-vars
+/* eslint-disable no-unused-vars */
 const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    const total = blogs
+        .map(blog => blog.likes)
+        .reduce((prev, cur) => {
+            return prev + cur
+        })
+    return total
+}
+
+const favoriteBlog = (blogs) => {
+    const favorite = blogs
+        .reduce((prev, cur) => {
+            return prev.likes > cur.likes ? prev : cur
+        })
+
+    return favorite
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes,
+    favoriteBlog
 }
