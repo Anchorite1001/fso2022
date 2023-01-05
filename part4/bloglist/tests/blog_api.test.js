cea987fc064bcc82832b5eb70f3d54f5c22ebb16
personal <yangwu.1415@gmail.com>
2023-01-05 18:29:36 +1100

    exercise 4.10

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -2,8 +2,18 @@ const _ = require('lodash')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const Blog = require('../models/blog')
+const testingLists = require('./BlogList')
 
 const api = supertest(app)
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+    
+    const blogObjects = testingLists.blogs.map(blog => new Blog(blog))
+    const promiseArray = blogObjects.map(blog => blog.save())
+    await Promise.all(promiseArray)
+})
 
 test('blogs are returned as json', async () => {
     await api
@@ -18,6 +28,31 @@ test('blog has unique identifier id', async () => {
 
     expect(testRandom.id).toBeDefined()
 })
+
+test('a new blog is added', async () => {
+    const newBlog = {
+        _id: '5a422a851b54a676234c17e7',
+        title: 'React patterns Renewal',
+        author: 'Michael Chan',
+        url: 'https://reactpatternsrenewal.com/',
+        likes: 7,
+        __v: 0
+    }
+
+    await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+    const response = await api.get('/api/blogs')
+    const titles = response.body.map( blog => blog.title)
+
+    expect(response.body).toHaveLength(testingLists.blogs.length + 1)
+    expect(titles).toContain(
+        'React patterns Renewal'
+    )
+}, 100000)
 
 afterAll(() => {
     mongoose.connection.close()
