f9e7bf42840ebcdd74cd8c414c95eb333c7f64f3
personal <yangwu.1415@gmail.com>
2023-01-06 23:43:53 +1100

    exercise 4.15

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 
 const config = require('./utils/config')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 
 mongoose.connect(config.MONGODB_URL)
     .then(() => console.log('Connected to MongoDB'))
@@ -14,5 +15,6 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 module.exports = app
