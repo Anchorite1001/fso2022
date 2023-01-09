45932046274a6e20da65bef41b7e8a6d17441eaf
personal <yangwu.1415@gmail.com>
2023-01-09 13:59:45 +1100

    exercise 4.18

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -7,6 +7,7 @@ const config = require('./utils/config')
 const middleware = require('./utils/middleware')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 mongoose.connect(config.MONGODB_URL)
     .then(() => console.log('Connected to MongoDB'))
@@ -17,6 +18,7 @@ app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.errorHandler)
 
