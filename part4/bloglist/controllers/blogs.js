df285db8f4cc7a72adfe77a40623c2f668e1c63e
personal <yangwu.1415@gmail.com>
2023-01-09 14:54:36 +1100

    exercise 4.19

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,5 +1,16 @@
+const jwt = require('jsonwebtoken')
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
+
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer')) {
+        return authorization.substring(7)
+    }
+
+    return null
+}
 
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({}, 'title author url likes').populate('user', {username: 1, name: 1})
@@ -12,7 +23,19 @@ blogsRouter.post('/', async (request, response) => {
         return response.status(400).end()
     }
 
+    const token = getTokenFrom(request)
+    if (!token) {
+        return response.status(401).json({ error: 'token missing'})
+    }
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if(!decodedToken.id) {
+        return response.status(401).json({ error: 'token invalid'})
+    }
+
+    const user = await User.findById(decodedToken.id)
     newBlog.likes = newBlog.likes ? newBlog.likes : 0
+    newBlog.user = user.id
+
     const blog = new Blog(newBlog)
     const savedBlog = await blog.save()
     response.status(201).json(savedBlog)
